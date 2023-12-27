23ceaf74621529c22aed00a766e31d3c94c539ad
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-27 05:30:08 -0500

    up

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Ruta de ejemplo
-app.get('/api/hola', (req, res) => {
-  res.json({ mensaje: '¡Hola desde tu API!' });
+// Rutas para cada sección
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Reemplaza 'index.html' con el nombre de tu archivo HTML principal
 });
 
+app.get('/apis', (req, res) => {
+  res.sendFile(__dirname + '/apis.html'); // Reemplaza 'apis.html' con el nombre de tu archivo HTML para la sección de APIs
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html'); // Reemplaza 'about.html' con el nombre de tu archivo HTML para la sección "Acerca de"
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html'); // Reemplaza 'contact.html' con el nombre de tu archivo HTML para la sección de contacto
+});
+
+// Puedes agregar más rutas según sea necesario para otras secciones
+
 app.listen(port, () => {
-  console.log(`La API está escuchando en http://localhost:${port}`);
+  console.log(`La API está escuchando`);
 });
