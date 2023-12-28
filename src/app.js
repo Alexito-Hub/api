7d03ae0a5eae0df105a03869d0516b87d7e17e57
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-28 00:13:38 -0500

    up to date

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 4000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// routes
+app.use(require('./routes'));
+// app.use('/api/movies', require('./routes/movies'));
+app.use('/api/config', require('./routes/config'));
+
+// Manejo de errors global
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
---
+++ src/routes/config.js
@@ -0,0 +1,49 @@
+require('../config')
+const { Router } = require('express');
+const fs = require('fs/promises');
+const path = require('path');
+const router = new Router();
+
+const configFilePath = path.join(__dirname, '../json/config.json');
+
+router.get('/', async (req, res) => {
+  try {
+    const configData = await fs.readFile(configFilePath, 'utf-8');
+    const config = JSON.parse(configData);
+    res.json({
+      creator: global.name,
+      status: 200,
+      result: config
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      creator: global.name,
+      status: 500,
+      result: { error: 'Error al obtener la configuración' }
+    });
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    const configData = await fs.readFile(configFilePath, 'utf-8');
+    let config = JSON.parse(configData);
+    config = { ...config, ...req.body };
+    await fs.writeFile(configFilePath, JSON.stringify(config, null, 2), 'utf-8');
+    res.json({
+      creator: global.name,
+      status: 200,
+      result: { message: 'Configuración actualizada con éxito' }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      creator: global.name,
+      status: 500,
+      result: { error: 'Error al actualizar la configuración' }
+    });
+  }
+});
+
+module.exports = router;
