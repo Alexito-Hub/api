fa4f75ee884cc34d06ff40ad36c6e42f17428a09
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-07 03:06:10 -0500

    update telegra.js

--- src/routers/api/telegra.js
+++ src/routers/api/telegra.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = new Router();
 const axios = require('axios');
 const fs = require('fs');
 
