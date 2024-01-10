ef27aac23699e82f1cec842da3cfbec59116ad35
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2024-01-10 18:01:23 -0500

    up

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('/contacts', (req, res) => {
 });
 
 app.get('/curalacha', (req, res) => {
-	res.sendFile(path.join(__dirname, '../', 'public', 'u.html'));
+	res.sendFile(path.join(__dirname, '../', 'public', 'curalacha.html'));
 });
 
 const DateKey = (req, res, next) => {
